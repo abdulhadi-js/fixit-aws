df6a5949815109bb49695b094a306a38f9aeeb2a
AbdulhadiDeveloper <dev.alhadi@gmail.com>
2026-07-14 13:40:54 +0500

    fix: replace deprecated dynamodb_table with use_lockfile in backend.tf

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,11 +1,10 @@
-# Remote state stored in S3, locked with DynamoDB
-# Run bootstrap/init-state.sh ONCE before `terraform init`
+# Remote state stored in S3, locked with native S3 lockfile (Terraform 1.15+)
 terraform {
   backend "s3" {
     bucket         = "fixit-terraform-state-ap-southeast-1"
     key            = "production/terraform.tfstate"
     region         = "ap-southeast-1"
     encrypt        = true
-    dynamodb_table = "fixit-terraform-locks"
+    use_lockfile   = true
   }
 }
